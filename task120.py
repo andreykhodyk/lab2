ec97f7dc114a1270f1c1ec033d24d9a51a2fd579
andreykhodyk <andreykkhodyk@gmail.com>
2026-09-21 16:48:37 +0300

    first commit

---
+++ task120.py
@@ -0,0 +1,10 @@
+distance = float(input())
+time_min = float(input())
+
+hours = time_min / 60
+speed = distance / hours
+
+if speed <= 60:
+    print("Traffic rules are executed.")
+else:
+    print("Traffic rules are not met.")
