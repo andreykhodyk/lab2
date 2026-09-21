2b7c1bb6e2f54189e4771ca2071a2e2c60b7a4d9
andreykhodyk <andreykkhodyk@gmail.com>
2026-09-21 18:07:34 +0300

    Add file task150.py

---
+++ task150.py
@@ -0,0 +1,18 @@
+last_name = input()
+first_name = input()
+age = input()
+hobby = input()
+phone = input()
+
+option = int(input())
+
+if option == 1:
+    print(last_name)
+elif option == 2:
+    print(first_name)
+elif option == 3:
+    print(age)
+elif option == 4:
+    print(hobby)
+elif option == 5:
+    print(phone)
